ed3285bc41a1003e2458dccd60054b777279c2d9
Mohammad Shamsi <mshamsi502.dev@gmail.com>
2025-04-22 13:06:44 +0330

    Initial Scoob Search commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mshamsi/scoob-search
+
+go 1.24.2
---
+++ main.go
@@ -0,0 +1,40 @@
+// package scoob
+package main
+
+import (
+	"fmt"
+	"github.com/mshamsi502/scoob-search/scoob"
+)
+
+type Scoob struct{}
+
+func (s Scoob) DoobySearch(query string, objects []string) []string {
+	var result []string
+	for _, obj := range objects {
+		if containsIgnoreCase(obj, query) {
+			result = append(result, obj)
+		}
+	}
+	return result
+}
+
+func containsIgnoreCase(text, substr string) bool {
+	return (len(text) >= len(substr)) && (stringMatch(text, substr))
+}
+
+func stringMatch(text, substr string) bool {
+	return stringIndex(text, substr) != -1
+}
+
+func stringIndex(text, substr string) int {
+	for i := 0; i <= len(text)-len(substr); i++ {
+		if text[i:i+len(substr)] == substr {
+			return i
+		}
+	}
+	return -1
+}
+
+func Test() {
+	fmt.Println("Scoob Package Ready 🐾")
+}
